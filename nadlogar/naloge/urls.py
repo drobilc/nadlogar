07bbe372480ab4875be225c339c416f77d32bedb
Niki Bizjak <drobilc@gmail.com>
2020-04-26 11:59:57 +0200

    Odstranil nepotrebne endpointe za naloge

--- nadlogar/naloge/urls.py
+++ nadlogar/naloge/urls.py
@@ -3,11 +3,4 @@ from django.urls import path
 from . import views
 
 app_name = 'naloge'
-urlpatterns = [
-    # primer: /naloge/
-    path('', views.index, name='index'),
-    # primer: /naloge/5/
-    path('<int:pk>/', views.podrobnosti, name='podrobnosti'),
-    # primer: /naloge/5/primer/
-    path('<int:pk>/primer/', views.primer, name='primer'),
-]
+urlpatterns = []
