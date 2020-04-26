eed99f82d0ac308b9e12c03cbe7ee95f166ee2da
Niki Bizjak <drobilc@gmail.com>
2020-04-26 11:33:31 +0200

    Dodal privzeto predlogo prikaza testov

--- nadlogar/testi/urls.py
+++ nadlogar/testi/urls.py
@@ -8,6 +8,4 @@ urlpatterns = [
     path('', views.index, name='index'),
     # ex: /testi/5/
     path('<int:pk>/', views.podrobnosti, name='podrobnosti'),
-    # ex: /testi/5/nadloga
-    path('<int:pk>/nadloga/', views.nadloga, name='nadloga'),
 ]
--- nadlogar/testi/views.py
+++ nadlogar/testi/views.py
@@ -2,17 +2,10 @@ from django.shortcuts import get_object_or_404, render
 
 from .models import Test
 
-
 def index(request):
     seznam_testov = Test.objects.all()
-    return render(request, 'testi/index.html', {'seznam_testov': seznam_testov})
-
+    return render(request, 'testi/seznam_testov.html', {'seznam_testov': seznam_testov})
 
 def podrobnosti(request, pk: int):
     test: Test = get_object_or_404(Test, pk=pk)
-    return render(request, 'testi/podrobnosti.html', {'test': test})
-
-
-def nadloga(request, pk: int):
-    test: Test = get_object_or_404(Test, pk=pk)
-    return render(request, 'testi/nadloga.html', {'test': test, 'nadloga': test.ustvari_nadlogo()})
+    return render(request, 'testi/test.html', {'test': test})
