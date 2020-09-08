854035f5cb12d023e6ce258ff5bd9aef4280530d
Niki Bizjak <drobilc@gmail.com>
2020-09-08 09:51:37 +0200

    Spremenjen izgled seznama primerov - pred primeri so dodane črke namesto krogcev

--- nadlogar/naloge/generatorji/html_generator.py
+++ nadlogar/naloge/generatorji/html_generator.py
@@ -16,23 +16,56 @@ class HtmlGenerator(Visitor):
     @staticmethod
     def generiraj_html(naloga):
         html_generator = HtmlGenerator()
-        return html_generator.visit_naloga(naloga, None)
+        generirano_drevo = html_generator.visit_naloga(naloga, None)
+        return tostring(generirano_drevo, xml_declaration=False, encoding='utf-8')
     
     def visit_naloga(self, naloga, argument):
         if isinstance(naloga, NalogaIzlociVsiljivca):
             return self.visit_izloci_vsiljivca_naloga(naloga)
+        elif isinstance(naloga, NalogaVstaviUstreznoObliko):
+            return self.visit_vstavi_ustrezno_obliko_naloga(naloga)
+        elif isinstance(naloga, NalogaDolociSlovnicnoStevilo):
+            return self.visit_doloci_slovnicno_stevilo_naloga(naloga)
+        elif isinstance(naloga, NalogaDolociSteviloPomenov):
+            return self.visit_stevilo_pomenov_naloga(naloga)
+        elif isinstance(naloga, NalogaGlasVsiljivec):
+            return self.visit_glas_vsiljivec_naloga(naloga)
         return None
     
     def visit_izloci_vsiljivca_naloga(self, naloga: NalogaIzlociVsiljivca):
-
         html = etree.Element('div')
-
         subelement_with_text(html, 'p', _text=naloga.navodila)
         
         seznam_primerov = etree.SubElement(html, 'ul')
-
         for primer in naloga.primeri():
                 primer_element = etree.SubElement(seznam_primerov, 'li')
                 primer_element.text = ', '.join(primer['besede'])
 
-        return tostring(html, xml_declaration=False, encoding='utf-8')
+        return html
+    
+    def visit_vstavi_ustrezno_obliko_naloga(self, naloga: NalogaVstaviUstreznoObliko):
+        html = etree.Element('div')
+        subelement_with_text(html, 'p', _text=naloga.navodila)
+        
+        seznam_primerov = etree.SubElement(html, 'ul')
+        for primer in naloga.primeri():
+            primer_element = etree.SubElement(seznam_primerov, 'li')
+            primer_tekst = '{} __________ ({}) {}'.format(primer['pred'], primer['iztocnica'], primer['po'])
+            primer_element.text = primer_tekst
+
+        return html
+    
+    def visit_doloci_slovnicno_stevilo_naloga(self, naloga: NalogaDolociSlovnicnoStevilo):
+        html = etree.Element('div')
+        subelement_with_text(html, 'p', _text=naloga.navodila)
+        return html
+    
+    def visit_stevilo_pomenov_naloga(self, naloga: NalogaDolociSteviloPomenov):
+        html = etree.Element('div')
+        subelement_with_text(html, 'p', _text=naloga.navodila)
+        return html
+    
+    def visit_glas_vsiljivec_naloga(self, naloga: NalogaGlasVsiljivec):
+        html = etree.Element('div')
+        subelement_with_text(html, 'p', _text=naloga.navodila)
+        return html
