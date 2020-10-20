3c0df96e24ae718de0d810b9dff040daa2f2cdef
Niki Bizjak <drobilc@gmail.com>
2020-10-20 19:01:46 +0200

    Popravil hrošča, kjer se seznam dokumentov ni prikazal, ker URL za odjavo ni obstajal

--- nadlogar/accounts/urls.py
+++ nadlogar/accounts/urls.py
@@ -6,6 +6,7 @@ app_name = 'racuni'
 
 urlpatterns = [
     path('prijava/', authentication_views.LoginView.as_view(template_name="registration/login.html"), name='prijava'),
+    path('odjava/', authentication_views.LogoutView.as_view(), name='odjava'),
     path('vpis/', views.registracija, name='registracija'),
     path('novogeslo/', views.pozabljeno_geslo, name='pozabljeno_geslo'),
 ]
